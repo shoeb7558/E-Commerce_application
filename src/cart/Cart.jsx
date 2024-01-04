8b5a28d796545613faad463db9cbf7bbe3d4a9fc
shoeb7558 <shoeb9530@gmail.com>
2024-01-04 16:44:44 +0530

    in this update I have created new cartitems

--- src/cart/Cart.jsx
+++ src/cart/Cart.jsx
@@ -2,23 +2,25 @@
 import React from 'react';
 import './CartModule.css';
 
-const Cart = ({ isOpen, onClose, datatransfer }) => {
+const Cart = ({ isOpen, onClose, cartItems }) => {
   return (
-  
     <div className={`cart ${isOpen ? 'open' : ''} cartdiv`}>
       <h2>Shopping Cart</h2>
-      {datatransfer.map((product, index) => (
-        <div key={index} className='CartItem'>
-          <h3>{product.title}</h3>
-          <p>Price: ${product.price}</p>
-          {/* Uncomment the following line when you have imageUrl */}
-          <img src={product.imageUrl} alt={product.title} />
-          {/* <button className='addtocartbutton'>Add To Cart</button> */}
-        </div>
-      ))}
-      <button className='closecart' onClick={onClose}>Close</button>
+      {cartItems.length > 0 ? (
+        cartItems.map((item, index) => (
+          <div key={index} className='CartItem'>
+            <h3>{item.title}</h3>
+            <p>Price: ${item.price}</p>
+            {/* <img src={item.imageUrl} alt={item.title} /> */}
+          </div>
+        ))
+      ) : (
+        <p>Your cart is empty</p>
+      )}
+      <button className='close' onClick={onClose}>
+        Close
+      </button>
     </div>
-  
   );
 };
 
--- src/components/Header_Element.jsx
+++ src/components/Header_Element.jsx
@@ -1,53 +1,12 @@
-import React from 'react'
-import './Header_ElementModule.css'
+// Header_ElementModule.js
+import React, { useState } from 'react';
+import './Header_ElementModule.css';
 import Cart from '../cart/Cart';
-import { useState } from 'react';
+import Cards from '../Cards/Cards';
 
-function Header_Element() {
-  const productsArr = [
-
-    {
-    
-    title: 'Colors',
-    
-    price: 100,
-    
-    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
-    
-    },
-    
-    {
-    
-    title: 'Black and white Colors',
-    
-    price: 50,
-    
-    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
-    
-    },
-    
-    {
-    
-    title: 'Yellow and Black Colors',
-    
-    price: 70,
-    
-    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
-    
-    },
-    
-    {
-    
-    title: 'Blue Color',
-    
-    price: 100,
-    
-    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
-    
-    }
-    
-    ]
+function Header_Element({ datatransfer }) {
   const [isCartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
 
   const openCart = () => {
     setCartOpen(true);
@@ -57,20 +16,29 @@ function Header_Element() {
     setCartOpen(false);
   };
 
+  const addToCart = (item) => {
+    setCartItems((prevItems) => [...prevItems, item]);
+  };
+
   return (
     <>
       <div className='headerdiv'>
-        <label  className='labels'>Home</label>
-        <label className='labels' >Store</label>
+        <label className='labels'>Home</label>
+        <label className='labels'>Store</label>
         <label className='labels'>About</label>
-        <button className='cartbutton' onClick={openCart}>Open Cart</button>
-        <Cart isOpen={isCartOpen} onClose={closeCart} datatransfer={productsArr}/>
+        <button className='cartbutton' onClick={openCart}>
+          Open Cart
+        </button>
+        <Cart isOpen={isCartOpen} onClose={closeCart} cartItems={cartItems} />
       </div>
       <div className='headerdiv2'>
         <h1>The Generics</h1>
       </div>
+      <div>
+        <Cards datatransfer={datatransfer} addToCart={addToCart} />
+      </div>
     </>
-  )
+  );
 }
 
-export default Header_Element
+export default Header_Element;
