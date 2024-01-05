461301ed91336005548196ee22ea715a9809a366
shoeb7558 <shoeb9530@gmail.com>
2024-01-05 17:54:04 +0530

    I have added fecth api function

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
-import './HomeModule.css'
+import React, { useEffect, useState } from 'react';
+import './HomeModule.css';
 
 function Home() {
+  const [data, setdata] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://swapi.dev/api/films/');
+        const result = await response.json();
+
+        setdata(result.results);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h1>this is home page</h1>
-     
+      <h1>This is the home page</h1>
+      <ul>
+        {data.map((film, index) => (
+          <li key={index} className='movieslist'>{film.title}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
